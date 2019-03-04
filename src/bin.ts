cc07c982332f826795a268eaa2f3c438118374de
masad-frost <farismasad@gmail.com>
2019-03-04 05:28:12 -0800

    Replit specific

--- src/bin.ts
+++ src/bin.ts
@@ -1,108 +1,30 @@
 #!/usr/bin/env node
 
-import { join, resolve } from 'path'
-import { start, Recoverable } from 'repl'
-import { inspect } from 'util'
-import Module = require('module')
-import arg = require('arg')
-import { diffLines } from 'diff'
-import { Script } from 'vm'
-import { readFileSync, statSync } from 'fs'
-import { register, VERSION, DEFAULTS, TSError, parse } from './index'
-
-const args = arg({
-  // Node.js-like options.
-  '--eval': String,
-  '--print': Boolean,
-  '--require': [String],
-
-  // CLI options.
-  '--files': Boolean,
-  '--help': Boolean,
-  '--version': arg.COUNT,
-
-  // Project options.
-  '--compiler': String,
-  '--compiler-options': parse,
-  '--project': String,
-  '--ignore-diagnostics': [String],
-  '--ignore': [String],
-  '--transpile-only': Boolean,
-  '--type-check': Boolean,
-  '--pretty': Boolean,
-  '--skip-project': Boolean,
-  '--skip-ignore': Boolean,
-
-  // Aliases.
-  '-e': '--eval',
-  '-p': '--print',
-  '-r': '--require',
-  '-h': '--help',
-  '-v': '--version',
-  '-T': '--transpile-only',
-  '-I': '--ignore',
-  '-P': '--project',
-  '-C': '--compiler',
-  '-D': '--ignore-diagnostics',
-  '-O': '--compiler-options'
-}, {
-  stopAtPositional: true
-})
-
-const {
-  '--help': help = false,
-  '--version': version = 0,
-  '--files': files = DEFAULTS.files,
-  '--compiler': compiler = DEFAULTS.compiler,
-  '--compiler-options': compilerOptions = DEFAULTS.compilerOptions,
-  '--project': project = DEFAULTS.project,
-  '--ignore-diagnostics': ignoreDiagnostics = DEFAULTS.ignoreDiagnostics,
-  '--ignore': ignore = DEFAULTS.ignore,
-  '--transpile-only': transpileOnly = DEFAULTS.transpileOnly,
-  '--type-check': typeCheck = DEFAULTS.typeCheck,
-  '--pretty': pretty = DEFAULTS.pretty,
-  '--skip-project': skipProject = DEFAULTS.skipProject,
-  '--skip-ignore': skipIgnore = DEFAULTS.skipIgnore
-} = args
-
-if (help) {
-  console.log(`
-Usage: ts-node [options] [ -e script | script.ts ] [arguments]
-
-Options:
-
-  -e, --eval [code]              Evaluate code
-  -p, --print                    Print result of \`--eval\`
-  -r, --require [path]           Require a node module before execution
-
-  -h, --help                     Print CLI usage
-  -v, --version                  Print module version information
-
-  -T, --transpile-only           Use TypeScript's faster \`transpileModule\`
-  -I, --ignore [pattern]         Override the path patterns to skip compilation
-  -P, --project [path]           Path to TypeScript JSON project file
-  -C, --compiler [name]          Specify a custom TypeScript compiler
-  -D, --ignore-diagnostics [code] Ignore TypeScript warnings by diagnostic code
-  -O, --compiler-options [opts]   JSON object to merge with compiler options
-
-  --files                        Load files from \`tsconfig.json\` on startup
-  --pretty                       Use pretty diagnostic formatter
-  --skip-project                 Skip reading \`tsconfig.json\`
-  --skip-ignore                  Skip \`--ignore\` checks
-`)
-
-  process.exit(0)
-}
-
-// Output project information.
-if (version === 1) {
-  console.log(`v${VERSION}`)
-  process.exit(0)
-}
+import { join, resolve } from 'path';
+import { start, Recoverable } from 'repl';
+import { inspect } from 'util';
+import Module = require('module');
+import arg = require('arg');
+import { diffLines } from 'diff';
+import { Script, createContext, runInContext } from 'vm';
+import { readFileSync, statSync } from 'fs';
+import { register, VERSION, DEFAULTS, TSError, parse } from './index';
+import { V4MAPPED } from 'dns';
 
 const cwd = process.cwd()
-const code = args['--eval']
-const isPrinted = args['--print'] !== undefined
+const help = false
+const version = 0
+const files = DEFAULTS.files
+const compiler = DEFAULTS.compiler
+const compilerOptions = DEFAULTS.compilerOptions
+const project = DEFAULTS.project
+const ignoreDiagnostics = DEFAULTS.ignoreDiagnostics
+const ignore = DEFAULTS.ignore
+const transpileOnly = DEFAULTS.transpileOnly
+const typeCheck = DEFAULTS.typeCheck
+const pretty = DEFAULTS.pretty
+const skipProject = DEFAULTS.skipProject
+const skipIgnore = DEFAULTS.skipIgnore
 
 // Register the TypeScript compiler instance.
 const service = register({
@@ -117,20 +39,9 @@ const service = register({
   compiler,
   ignoreDiagnostics,
   compilerOptions,
-  readFile: code ? readFileEval : undefined,
-  fileExists: code ? fileExistsEval : undefined
+  readFile: readFileEval,
+  fileExists: fileExistsEval
 })
-
-// Output project information.
-if (version >= 2) {
-  console.log(`ts-node v${VERSION}`)
-  console.log(`node ${process.version}`)
-  console.log(`compiler v${service.ts.version}`)
-  process.exit(0)
-}
-
-// Require specified modules before start-up.
-if (args['--require']) (Module as any)._preloadModules(args['--require'])
 
 /**
  * Eval helpers.
@@ -139,59 +50,9 @@ const EVAL_FILENAME = `[eval].ts`
 const EVAL_PATH = join(cwd, EVAL_FILENAME)
 const EVAL_INSTANCE = { input: '', output: '', version: 0, lines: 0 }
 
-// Prepend `ts-node` arguments to CLI for child processes.
-process.execArgv.unshift(__filename, ...process.argv.slice(2, process.argv.length - args._.length))
-process.argv = [process.argv[1]].concat(args._.length ? resolve(cwd, args._[0]) : []).concat(args._.slice(1))
-
 // Execute the main contents (either eval, script or piped).
-if (code) {
-  evalAndExit(code, isPrinted)
-} else {
-  if (args._.length) {
-    Module.runMain()
-  } else {
-    // Piping of execution _only_ occurs when no other script is specified.
-    if (process.stdin.isTTY) {
-      startRepl()
-    } else {
-      let code = ''
-      process.stdin.on('data', (chunk: Buffer) => code += chunk)
-      process.stdin.on('end', () => evalAndExit(code, isPrinted))
-    }
-  }
-}
 
-/**
- * Evaluate a script.
- */
-function evalAndExit (code: string, isPrinted: boolean) {
-  const module = new Module(EVAL_FILENAME)
-  module.filename = EVAL_FILENAME
-  module.paths = (Module as any)._nodeModulePaths(cwd)
-
-  ;(global as any).__filename = EVAL_FILENAME
-  ;(global as any).__dirname = cwd
-  ;(global as any).exports = module.exports
-  ;(global as any).module = module
-  ;(global as any).require = module.require.bind(module)
-
-  let result: any
-
-  try {
-    result = _eval(code)
-  } catch (error) {
-    if (error instanceof TSError) {
-      console.error(error.diagnosticText)
-      process.exit(1)
-    }
-
-    throw error
-  }
-
-  if (isPrinted) {
-    console.log(typeof result === 'string' ? result : inspect(result))
-  }
-}
+startRepl()
 
 /**
  * Evaluate the code snippet.
@@ -236,14 +97,20 @@ function exec (code: string, filename: string) {
  * Start a CLI REPL.
  */
 function startRepl () {
+  const mainFile = readFileSync('/home/runner/index.ts').toString('utf-8')
+  _eval(mainFile)
+
   const repl = start({
-    prompt: '\u001b[33m\u001b[00m ',
+    // \u001b[33m\u001b[00m
+    prompt: '> ',
     input: process.stdin,
     output: process.stdout,
     terminal: process.stdout.isTTY,
     eval: replEval,
     useGlobal: true
   })
+
+
 
   // Bookmark the point where we should reset the REPL state.
   const resetEval = appendEval('')
@@ -267,7 +134,11 @@ function startRepl () {
       }
 
       const undo = appendEval(identifier)
-      const { name, comment } = service.getTypeInfo(EVAL_INSTANCE.input, EVAL_PATH, EVAL_INSTANCE.input.length)
+      const { name, comment } = service.getTypeInfo(
+        EVAL_INSTANCE.input,
+        EVAL_PATH,
+        EVAL_INSTANCE.input.length
+      )
 
       undo()
 
@@ -280,7 +151,12 @@ function startRepl () {
 /**
  * Eval code from the REPL.
  */
-function replEval (code: string, _context: any, _filename: string, callback: (err: Error | null, result?: any) => any) {
+function replEval (
+  code: string,
+  _context: any,
+  _filename: string,
+  callback: (err: Error | null, result?: any) => any
+) {
   let err: Error | null = null
   let result: any
 
@@ -318,7 +194,11 @@ function appendEval (input: string) {
   const undoLines = EVAL_INSTANCE.lines
 
   // Handle ASI issues with TypeScript re-evaluation.
-  if (undoInput.charAt(undoInput.length - 1) === '\n' && /^\s*[\[\(\`]/.test(input) && !/;\s*$/.test(undoInput)) {
+  if (
+    undoInput.charAt(undoInput.length - 1) === '\n' &&
+    /^\s*[\[\(\`]/.test(input) &&
+    !/;\s*$/.test(undoInput)
+  ) {
     EVAL_INSTANCE.input = `${EVAL_INSTANCE.input.slice(0, -1)};\n`
   }
 
@@ -357,7 +237,9 @@ function readFileEval (path: string) {
 
   try {
     return readFileSync(path, 'utf8')
-  } catch (err) {/* Ignore. */}
+  } catch (err) {
+    /* Ignore. */
+  }
 }
 
 /**
